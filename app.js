8933b06cd7ecf5d1570d13f25360fd837a9cebb7
Andrea Harrison <Andrealharrison@gmail.com>
2023-01-25 17:18:12 -0800

    assign2

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var homeRouter = require('./routes/home');
+//var homeRouter = require('./routes/home');
+var homeRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const { authCheck } = require('./auth/authCheck')
 
