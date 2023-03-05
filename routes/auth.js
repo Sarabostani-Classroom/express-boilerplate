21dd38574a3ffc64b0de8f04266b7f618aaa5ecd
ISAAK WHEELER <isaak.wheeler@hotmail.com>
2023-03-05 12:39:28 -0800

    user sign up works

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,43 @@
 var express = require('express');
 var router = express.Router();
+/*
+Postman /signup:
+    body:
+        setting: raw -> json 
+    
+    {
+        "username": "isaak",
+        "password": "Wheeler"
+    }
+*/
+
+
+let users = [
+    {
+        "username": "admin",
+        "password": "admin",
+    }
+];
 
 router.post('/signup', (req, resp, next) => {
+    if (!req.body || !req.body.username || !req.body.password)
+    {
+        resp.send(401, 'Bad request')
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password,
+    });
+    console.log("Req Body: " + req.body);
+    console.log("All Users: ");
+    users.forEach(element => {
+        console.log("Username: " + element.username + " Password: " + element.password);
+    });
+
     resp.send("signing up");
 });
 
