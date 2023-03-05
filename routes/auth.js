5c9247aa3efb7f9f67092241d05955ce7eddcf5a
ISAAK WHEELER <isaak.wheeler@hotmail.com>
2023-03-05 13:02:55 -0800

    adding incrementing IDs to users

--- routes/auth.js
+++ routes/auth.js
@@ -14,34 +14,59 @@ Postman /signup:
 
 let users = [
     {
+        "id": "1",
         "username": "admin",
         "password": "admin",
     }
 ];
 
+
 router.post('/signup', (req, resp, next) => {
     if (!req.body || !req.body.username || !req.body.password)
     {
-        resp.send(401, 'Bad request')
+        resp.send(401, 'Bad request');
     }
-
+    
     const username = req.body.username;
     const password = req.body.password;
+    let userId = 1;
+
+    users.forEach(element => {
+        if (userId <= parseInt(element.id))
+        {
+            userId = userId + 1;
+        }
+    });
+
+    console.log("1");
+    
 
     users.push({
+        id: userId,
         username: username,
         password: password,
     });
     console.log("Req Body: " + req.body);
     console.log("All Users: ");
     users.forEach(element => {
-        console.log("Username: " + element.username + " Password: " + element.password);
+        console.log("Username: " + element.username + " Password: " + element.password + " ID: " + element.id);
     });
 
     resp.send("signing up");
 });
 
 router.post('/login', (req, resp, next) => {
+    if (!req.body || !req.body.username || !req.body.password)
+    {
+        resp.send(401, 'Bad request')
+    }
+
+    users.forEach(element => {
+        if ((element.username = req.body.username) && (element.password = req.body.password)){
+            resp.send()
+        }
+    });
+
     resp.send("loging in");
 });
 
